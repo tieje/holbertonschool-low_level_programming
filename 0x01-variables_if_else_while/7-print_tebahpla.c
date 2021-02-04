3840b761332a24bb342d8df19d4dbf6fecb90ad5
tieje <toj320@gmail.com>
2021-02-04 17:05:05 -0500

    gottem

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - betty don't kill me
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar(10);
+	return (0);
+}
