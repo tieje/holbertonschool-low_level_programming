4ea8019c90618c58f49ac1734b924e6eb299978a
tieje <toj320@gmail.com>
2021-02-04 18:09:30 -0500

    gottem

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -5,5 +5,26 @@
  */
 int main(void)
 {
-	
+	int x = 0;
+	int y = 0;
+
+	while (x != '9')
+	{
+		while (y != '9')
+		{
+			putchar(x);
+			putchar(y);
+			if (x == '9' && y == '9')
+			{
+				continue;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			y++;
+		}
+		x++;
+	}
 }
