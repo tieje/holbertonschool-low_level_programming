9d56ea05ab949cc17c4ff2a3c091a34cc8e89839
tieje <toj320@gmail.com>
2021-02-04 16:59:40 -0500

    gottem

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - betty don't kill me
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	putchar(10);
+	return (0);
+}
