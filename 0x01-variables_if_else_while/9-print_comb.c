0cdd8d2817ad19e34bc174395bb95ab6cf5a2728
tieje <toj320@gmail.com>
2021-02-04 17:38:57 -0500

    gottem

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - betty don't kill me
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+		if (x == 9)
+		{
+			continue;
+		}
+		putchar(44);
+	}
+	putchar(10);
+	return (0);
+}
