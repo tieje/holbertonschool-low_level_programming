e5d46e02ce3b5b1d3d03d7358f646131f4ff0ada
tieje <toj320@gmail.com>
2021-02-04 17:16:03 -0500

    gottem

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - betty don't kill me
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	for (x = 'a'; x <= 'f'; x++)
+	{
+		putchar(x);
+	}
+	putchar(10);
+	return (0);
+}
