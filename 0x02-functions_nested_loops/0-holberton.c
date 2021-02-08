b77678d48a805821d5490c29306b41b9ca11c0ea
tieje <toj320@gmail.com>
2021-02-08 16:51:59 -0500

    1

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Betty don't kill me
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("Holberton\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * main - betty don't kill me
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
