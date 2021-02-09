d69f66517d5b020319aa70105c4c33282cfbd25d
tieje <toj320@gmail.com>
2021-02-09 11:59:22 -0500

    gottem

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,3 +1,5 @@
+#include "holberton.h"
+
 /**
  * jack_bauer - print every minute of hte day starting from 00:00 to 23:59
  * Return: void
@@ -5,10 +7,35 @@
 
 void jack_bauer(void)
 {
-	int th;
 	int hh;
-	int tm;
 	int mm;
 
-	
+	for (hh = 0; hh <= 23; hh++)
+	{
+		for (mm = 0; mm <= 59; mm++)
+		{
+			if (hh > 10)
+			{
+				_putchar((hh / 10) + '0');
+				_putchar((hh % 10) + '0');
+			}
+			else
+			{
+				_putchar('0');
+				_putchar(hh + '0');
+			}
+			_putchar(':');
+			if (mm > 10)
+			{
+				_putchar((mm / 10) + '0');
+				_putchar((mm % 10) + '0');
+			}
+			else
+			{
+				_putchar('0');
+				_putchar(mm + '0');
+			}
+			_putchar(10);
+		}
+	}
 }
