b3e03d98da10d660a8b2bb22bdf77746ef2f1b33
tieje <toj320@gmail.com>
2021-02-11 14:51:42 -0500

    8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,4 +2,28 @@
 
 /**
  * print_square - prints a square boi
- * 
+ * @size: size of square
+ * Return: None
+ */
+
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	if (size <= 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+		for (y = 0; y < size ; y++)
+		{
+			for (x = 0; x < size; x++)
+			{
+				_putchar('#');
+			}
+			_putchar(10);
+		}
+	}
+}
