26e439c3c802be00e1c6b893a499a2791ca9cde6
tieje <toj320@gmail.com>
2021-02-11 16:05:35 -0500

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - print this triangle boi
+ * size: size of triangle
+ * Return: none
+ */
+
+void print_triangle(int size)
+{
+	int x;
+	int y;
+	int pound = 1;
+
+	if (size <= 0 )
+	{
+		_putchar(10);
+	}
+	else
+	{
+		for (y = 0; y < size; y++)
+		{
+			for (x = 0; x < size; x++)
+			{
+				if (size - x <= pound)
+				{
+					_putchar('#');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			pound++;
+			_putchar(10);
+		}
+	}
+}
