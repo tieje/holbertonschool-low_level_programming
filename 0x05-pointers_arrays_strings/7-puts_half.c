fa380178e6356c30a9c4c4ef7672c6c0e54e2995
tieje <toj320@gmail.com>
2021-02-16 14:38:05 -0500

    gottem

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,29 +8,16 @@
 
 void puts_half(char *str)
 {
-	int str_len = 0;
+	int str_len;
 	int x;
 
 	for (str_len = 0; str[str_len] != 0; str_len++)
 	{
 		continue;
 	}
-	/*
-	 *if (str_len % 2 == 0)
-	 *{
-	*/
 	for (x = str_len / 2; x < str_len; x++)
 	{
 		_putchar(str[x]);
 	}
-	/*
-	 *else
-	*{
-	*	for (x = (str_len - 1) / 2; x < str_len; x++)
-	*	{
-	*		_putchar(str[x]);
-	*	}
-	*}
-*/
 	_putchar(10);
 }
