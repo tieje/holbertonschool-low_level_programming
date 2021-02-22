d40bcfb00731351d50ea4e0f7111666b8aa3be42
tieje <toj320@gmail.com>
2021-02-22 13:35:52 -0500

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,7 +6,7 @@
  * Return: number of bytes in the initial segment of s
  */
 
-unsigned int _strspn( char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int x;
 	unsigned int y;
