b4bfaa13befbda628a471ea3991b15bfba4975a5
tieje <toj320@gmail.com>
2021-02-24 13:24:59 -0500

    3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+/**
+ * factorial - returns the factorial of a number
+ * @n: integer
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n > 0)
+	{
+		return (n * (factorial(n - 1)));
+	}
+	return (-1);
+}
