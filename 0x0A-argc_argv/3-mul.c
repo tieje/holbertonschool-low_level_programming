387e7664a8e1d31501b52c4fbbfafdea35bf5c13
tieje <toj320@gmail.com>
2021-02-27 15:20:52 -0500

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "holberton.h"
-
+#include <string.h>
+#include <stdlib.h>
 /**
  * main - prints the program's name
  * @argc: number of arguments
  * @argv: arguments in string
  * Return: 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i = atoi(argv[1]) * atoi(argv[2]);
 
