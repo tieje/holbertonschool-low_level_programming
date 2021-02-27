5f3fe167b8a42fcb15619ff2efcaa683979517ae
tieje <toj320@gmail.com>
2021-02-27 15:25:24 -0500

    fixed 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,12 +10,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int i = atoi(argv[1]) * atoi(argv[2]);
+	int i;
 
-	if (argc < 2)
+	if (argc < 3)
 	{
 		printf("Error\n");
+		return (0);
 	}
+	i = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", i);
 	return (0);
 }
