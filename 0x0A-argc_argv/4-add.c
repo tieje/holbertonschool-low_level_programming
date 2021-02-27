251e48f530f2e876a3712a973ece7b9409b36333
tieje <toj320@gmail.com>
2021-02-27 15:45:26 -0500

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include "holberton.h"
-
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 /**
  * main - prints the program's name
  * @argc: number of arguments
  * @argv: arguments in string
  * Return: 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int x;
 	int temp;
 	int res = 0;
 
-	for (x = 1; x <= argc; x++)
+	for (x = 1; x < argc; x++)
 	{
-		if (isdigit(atoi(argv[x])))
+		if (atoi(argv[x]))
 		{
 			temp = atoi(argv[x]);
-			res += temp
+			res += temp;
 		}
 		else
 		{
@@ -26,6 +28,6 @@ int main (int argc, char *argv[])
 			return (1);
 		}
 	}
-	printf("%d", argv[x]);
+	printf("%d\n", res);
 	return (0);
 }
