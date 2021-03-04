714ef0b19b056b1004c9c6b10d24a8c72f7201d8
tieje <toj320@gmail.com>
2021-03-04 13:36:01 -0500

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,5 +6,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	
+	/*unsigned int x;*/
+	void *a;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	a = malloc(nmemb * size);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	return (a);
 }
