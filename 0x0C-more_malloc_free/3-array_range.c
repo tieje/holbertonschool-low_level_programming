2ea1ea35d25a242f5aaee0d7dc75f2423ef45bf5
tieje <toj320@gmail.com>
2021-03-04 14:07:41 -0500

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -7,5 +7,22 @@
  */
 int *array_range(int min, int max)
 {
-	
+	int arr_iter = 0;
+	int *arr;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	arr = malloc((max - min) * sizeof(int) + 1);
+	if (arr != NULL)
+	{
+		for (arr_iter = 0; arr_iter <= max - min; arr_iter++)
+		{
+			arr[arr_iter] = min;
+			min++;
+		}
+		return (arr);
+	}
+	return (NULL);
 }
