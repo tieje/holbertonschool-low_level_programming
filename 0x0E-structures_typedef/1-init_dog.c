8bf6d23393760ff4ea11c26672f9155f413c8021
tieje <toj320@gmail.com>
2021-03-08 14:12:20 -0500

    update dog.h. Also 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include "dog.h"
 /**
  * init_dog = initialize type dog
  * @d: struct
@@ -7,5 +8,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
