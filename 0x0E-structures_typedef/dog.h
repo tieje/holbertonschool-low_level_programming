ee19f14faba3ddb0b7b8e72da47ec758e67f4777
tieje <toj320@gmail.com>
2021-03-08 13:57:28 -0500

    betty fix 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - dog description
+ * @name: dog name
+ * @age: dog age
+ *@owner: dog owner
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
