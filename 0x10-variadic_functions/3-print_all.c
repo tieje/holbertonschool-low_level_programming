b9e7c933346337fac4b093a7d446af80e666b8e0
tieje <toj320@gmail.com>
2021-03-11 22:44:53 -0500

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,11 +1,51 @@
-#include "variadic_functions.h"
-#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
 /**
- * print_all - print anything
- * @format: some format
- * @...: multiple arguments
+ * print_all - Prints anything
+ * @format: A list of chars representing data types
+ * Return: Void
  */
 void print_all(const char * const format, ...)
 {
-	
+	va_list arguments;
+	char *string;
+	int delimit = 0;
+	unsigned int i = 0;
+	char f;
+
+	va_start(arguments, format);
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+			delimit = 1;
+			printf("%c", va_arg(arguments, int));
+			break;
+		case 'i':
+			delimit = 1;
+			printf("%d", va_arg(arguments, int));
+			break;
+		case 'f':
+			delimit = 1;
+			printf("%f", va_arg(arguments, double));
+			break;
+		case 's':
+			delimit = 1;
+			string = va_arg(arguments, char *);
+			if (!string)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", string);
+		}
+		i++;
+		f = format[i];
+		if (delimit && (f == 'c' || f == 'i' || f == 'f' || f == 's'))
+			printf(", ");
+	}
+	printf("\n");
+	va_end(arguments);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,5 +6,14 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+/**
+ * struct print_s - struct for printing s
+ * @letter: something
+ * @f: function pointer
+ */
+typedef struct print_s
+{
+	char letter;
+	void (*f)();
+} print_t;
 #endif
