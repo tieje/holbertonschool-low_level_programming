fd470517cf295e20e175c4c3a2f05eae754fac1e
tieje <toj320@gmail.com>
2021-03-23 19:36:21 -0400

    case zero for 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,6 +26,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			iter++;
 		}
 	}
+	if (idx == 0)
+	{
+		new_node->next = current_node;
+		*head = new_node;
+		return (new_node);
+	}
 	if (current_node == NULL)
 		return (NULL);
 	new_node->next = current_node->next;
