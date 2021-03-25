55b5f4ad470ce0388fa1a74e786c119f38aec22d
tieje <toj320@gmail.com>
2021-03-25 15:21:05 -0400

    0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+
+/**
+ * _pow - power
+ * @a: integer to pow
+ * @b: exponent
+ * Return: unsigned int final integer
+ */
+unsigned int _pow(unsigned int a, unsigned int b)
+{
+	unsigned int i;
+	unsigned int res = 1;
+
+	if (b == 0)
+	{
+		return (1);
+	}
+	for (i = 1; i <= b; i++)
+	{
+		res = res * a;
+	}
+	return (res);
+}
+
+/**
+ * binary_to_uint - binary to unsigned int
+ * @b: the string of 0 and 1 characters
+ * Return: unsigned int integer
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int b_len;
+	unsigned int iter;
+	unsigned int result = 0;
+	unsigned int power;
+
+	for (b_len = 0; b[b_len]; b_len++)
+		continue;
+	power = b_len - 1;
+	for (iter = 0; iter < b_len; iter++)
+	{
+		if (b[iter] == '1')
+		{
+			result = result + _pow(2, power);
+		}
+		else if (b[iter] != '0')
+		{
+			return (0);
+		}
+		power--;
+	}
+	return (result);
+}
