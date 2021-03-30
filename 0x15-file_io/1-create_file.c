794073796ec209da230769b859d1ce5eff793044
tieje <toj320@gmail.com>
2021-03-30 17:32:52 -0400

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
 	if (fd == -1)
 		return (-1);
 	for (iter = 0; text_content[iter]; iter++)
