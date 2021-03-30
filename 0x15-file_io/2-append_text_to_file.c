3101e5faea18b0c58eed3b07d4c7e3bcd35c4e49
tieje <toj320@gmail.com>
2021-03-30 17:49:53 -0400

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,5 +7,22 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	
+	int fd, write_status, iter;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	for (iter = 0; text_content[iter]; iter++)
+	{
+		continue;
+	}
+	write_status = write(fd, text_content, iter);
+	if (write_status == -1)
+		return (-1);
+	close(fd);
+	return (1);
 }
