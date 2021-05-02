f318dde6bde52009ca84f921e2db985e3260ee42
tieje <toj320@gmail.com>
2021-05-02 16:25:31 -0400

    gottem

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,25 @@
 #include "lists.h"
 /**
+ * checkbounds - checks bounds of dlinked list
+ * @h: head
+ * @idx: index
+ * Return: 0 or 1
+ */
+int checkbounds(dlistint_t **h, unsigned int idx)
+{
+	dlistint_t *node = *h;
+	unsigned int counter = 0;
+
+	while (node)
+	{
+		node = node->next;
+		counter++;
+	}
+	if (idx <= counter)
+		return (0);
+	return (1);
+}
+/**
  * insert_dnodeint_at_index - inserts node at index in dlist
  * @h: head
  * @idx: index
@@ -10,31 +30,31 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	dlistint_t *current_node = *h;
-	unsigned int iter = 0;
+	unsigned int bounds = 0;
 
+	if (checkbounds(h, idx) == 1)
+		return (NULL); 
 	if (!h || new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	if (idx > 0)
-	{
-		while (iter != idx - 1 && current_node != NULL)
-		{
-			current_node = current_node->next;
-			iter++;
-		}
-	}
 	if (idx == 0)
 	{
-		new_node->next = current_node;
-		new_node->prev = NULL;
-		(*h)->prev = new_node;
-		*h = new_node;
-		return (new_node);
+		return (add_dnodeint(h, n));
+	}
+	while (current_node != NULL && bounds < idx)
+	{
+		current_node = current_node->next;
+		bounds++;
 	}
 	if (current_node == NULL)
-		return (NULL);
-	new_node->next = current_node->next;
-	current_node->next = new_node;
-	new_node->prev = current_node;
-	return (new_node);
+		return (add_dnodeint_end(h, n));
+	else
+	{
+		new_node->next = current_node;
+		new_node->prev = current_node->prev;
+		current_node->prev->next = new_node;
+		current_node->prev = new_node;
+		return (new_node);
+	}
+	return (NULL);
 }
