9661722967c1e01ad6e56010e4d360edeab16ac9
tieje <toj320@gmail.com>
2021-05-02 13:38:50 -0400

    do et

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -6,5 +6,17 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
+	dlistint_t *node = head;
+	int result = 0;
 
+	if (head == NULL)
+	{
+		return (0);
+	}
+	while (node)
+	{
+		result = result + node->n;
+		node = node->next;
+	}
+	return (result);
 }
