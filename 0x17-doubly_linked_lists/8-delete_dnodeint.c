d4e5e68d6958944c76884f0bdcba75156c97e1b3
tieje <toj320@gmail.com>
2021-05-02 16:32:10 -0400

    gottem

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,5 +7,32 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	unsigned int iter = 0;
+	dlistint_t *delete_node;
+	dlistint_t *prev_node = *head;
+	dlistint_t *next_node;
 
+	if (index > 0)
+	{
+		while (iter != index - 1 && prev_node != NULL)
+		{
+			prev_node = prev_node->next;
+			iter++;
+		}
+	}
+	if (prev_node == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		next_node = prev_node->next;
+		*head = next_node;
+		free(prev_node);
+		return (1);
+	}
+	delete_node = prev_node->next;
+	next_node = delete_node->next;
+    delete_node->next->prev = next_node;
+	prev_node->next = next_node;
+	free(delete_node);
+	return (1);
 }
