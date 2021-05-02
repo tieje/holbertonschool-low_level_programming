862587a21b44c61df4b4c3e30971a656cbd03a7f
tieje <toj320@gmail.com>
2021-05-02 12:29:18 -0400

    new node prev

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -31,6 +31,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		current_node = current_node->next;
 	}
 	current_node->next = new_node;
+	new_node->prev = current_node;
 	return (new_node);
 
 }
