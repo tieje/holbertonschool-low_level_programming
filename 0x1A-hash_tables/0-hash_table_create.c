9a41124152bf510f5f6da9d70e0f4995814aea04
tieje <toj320@gmail.com>
2021-05-27 15:12:29 -0400

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,5 +6,12 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    
+    hash_table_t *ht = malloc(size);
+
+    if (!ht)
+    {
+        return (NULL);
+    }
+    ht->size = 500;
+    return (ht);
 }
