469c9477f0e078fd530397a2573da5b42fc37ea2
tieje <toj320@gmail.com>
2021-05-28 13:06:44 -0400

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,10 +7,16 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
-	hash_node_t **hn = calloc(size, sizeof(hash_node_t *));
+	hash_node_t **hn;
 
-	if (!ht || !size)
+	if (!size)
+		return (NULL);
+	if (!ht)
+		return (NULL);
+	hn = calloc(size, sizeof(hash_node_t *));
+	if (!hn)
 	{
+		free(ht);
 		return (NULL);
 	}
 	ht->size = size;
