e3b4e485edc5b8edff0923248b851ce405605a2f
tieje <toj320@gmail.com>
2021-08-31 12:16:13 -0400

    there

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,12 +11,15 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
+	if (array)
 	{
-		printf("Value checked array[%zu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			printf("Value checked array[%zu] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				return (i);
+			}
 		}
 	}
 	return (-1);
